d7efb39d095a428d3019069ebf517e80316a7f73
saperez <saperez@unicauca.edu.co>
2018-12-24 04:12:49 -0500

    dataBinding added

--- app/src/main/java/com/cluster/android/projectkotlincluster/MainActivity.kt
+++ app/src/main/java/com/cluster/android/projectkotlincluster/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.cluster.android.projectkotlincluster
 
-import android.support.v7.app.AppCompatActivity
+
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.cluster.android.projectkotlincluster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main )
+
+
+
+
     }
 }
