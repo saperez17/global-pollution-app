ce74e800155519610d71a0261273ad4089b98b3e
saperez <saperez@unicauca.edu.co>
2018-12-24 06:40:33 -0500

    fixing strings 1

---
+++ app/src/main/java/com/cluster/android/projectkotlincluster/DescriptionAcrivity.kt
@@ -0,0 +1,12 @@
+package com.cluster.android.projectkotlincluster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DescriptionAcrivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_description_acrivity)
+    }
+}
